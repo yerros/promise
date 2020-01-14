5d792bd0f69556f5bb21b1c5b5758a9dd09e08ad
yerros <yerisrifan@icloud.com>
2020-01-14 16:07:10 +0700

    fix 02

--- 02.js
+++ 02.js
@@ -1,26 +1,21 @@
 const axios = require('axios');
 
-let Users = []
-let Posts = []
-
-const post = async () =>  await axios.get('https://jsonplaceholder.typicode.com/posts')
-.then(res => {
-    Posts.push(res.data)
-})
-
-const user = async () =>  await axios.get('https://jsonplaceholder.typicode.com/users')
-.then(res => {
-    Users.push(res.data)
-})
-
-
-const getPost = () => {
-    post();
-    user()
-    console.log(Users)
-    console.log(Posts);
-    //console.log(Posts)
-    
+const getUser = () => {
+    return new Promise()
 }
 
-getPost();
+(async function() {
+    const post = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const user = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    const resultPost = post.data;
+    const resultUser = user.data;
+
+    const result = resultPost.map(post => {
+        return {
+            ...post,
+            user: resultUser.find(user => user.id == post.userId)
+        }
+    })
+    console.log(result)
+})();
